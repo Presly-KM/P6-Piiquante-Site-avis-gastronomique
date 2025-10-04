08c39df3b51f23035774b52824c4529e3c7359d4
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-04 17:11:14 +0200

    express implement

--- back/index.js
+++ back/index.js
@@ -1,1 +1,8 @@
-console.log('Il s\'agit actuellement du fichier back/index.js')
+const express = require('express');
+const app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.listen(3000);
