44d3ca1a27f4d657dd9323367d32e596288c2da2
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-10 14:28:39 +0200

    adding function to find user and password in database (userInDb + passwordInDb)

--- back/server.js
+++ back/server.js
@@ -23,12 +23,10 @@ const users = []; // Ceci est un tableau en mémoire pour stocker les utilisateu
 
 function signUp(req, res) {
     const body = req.body;                            // Ici on récupère le corps de la requête c'est-à-dire les données envoyées par le client. Par exemple, dans une requête POST, le corps peut contenir des informations telles que le nom d'utilisateur, le mot de passe, etc. Express ne parviendra pas a lire le corps de la requête (ex: undefined) sans un middleware comme body-parser ou express.json().
-    console.log("body:", body);                       // On affiche le corps de la requête dans la console pour le débogageconst email = req.body.email;                     // On extrait l'email du corps de la requête.
     const email = req.body.email;                     // On extrait l'email du corps de la requête.
     const password = req.body.password;               // On extrait le mot de passe du corps de la requête.
     
     const userInDb = users.find((user) => user.email === email); // On vérifie si un utilisateur avec le même email existe déjà dans le tableau des utilisateurs.
-    console.log("userInDb:", userInDb);           // On affiche l'utilisateur trouvé dans la console pour le débogage.
     if (userInDb != null) {
         res.status(400).send("Utilisateur déjà existant avec cet email."); // Si un utilisateur avec le même email existe déjà, on renvoie une erreur 400 (Bad Request) au client.
         return;                                                           // On arrête l'exécution de la fonction.
@@ -47,14 +45,19 @@ function signUp(req, res) {
 function login(req, res) {
     const body = req.body;
     console.log("body:", body);
-    if (body.email !== "president@piiquante.com") {
-        res.status(401).send("Mauvais identifiants");
+    console.log("users in db:", users);
+
+    const userInDb = users.find((user) => user.email === body.email);
+    if (userInDb == null) {
+        res.status(401).send("Mauvais email");
         return;
     }
-    if (body.password !== "president") {
-        res.status(401).send("Mauvais identifiants");
+    const passwordInDb = userInDb.password;
+    if (passwordInDb !== body.password) {
+        res.status(401).send("Mauvais mot de passe temporaire");
         return;
     }
+
 // SIMULATION - À remplacer plus tard par la vraie logique
     res.status(200).json({
         userId: "dummyUser123",
