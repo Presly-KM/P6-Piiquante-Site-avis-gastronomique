a8232b9aefaa87ad86ee37816ff9d1f472c1626f
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-22 13:39:54 +0200

    configuration du stockage des fichiers (images) uploadées (ajout d'extension + nom du fichier etc.)

---
+++ back/middlewares/multer.js
@@ -0,0 +1,16 @@
+const multer = require('multer');                  // Importation du middleware multer pour destiné à gérer fichiers entrants (images)  
+
+const storage = multer.diskStorage({                // Configuration du stockage des fichiers uploadés (cf modele sur le site de multer)
+    destination: function (req, file, cb) {
+        cb(null, 'uploads');                       // Dossier de destination
+    },
+    filename: function (req, file, cb) {
+        const fileName = file.originalname.toLowerCase().split(' ').join('-'); // Nom du fichier en minuscule et sans espaces
+        cb(null, Date.now() + '-' + fileName); // Nom du fichier
+    }
+});
+const upload = multer({                            // Initialisation de multer avec la configuration de stockage définie ci-dessus
+    storage: storage
+});
+
+module.exports = { upload };
