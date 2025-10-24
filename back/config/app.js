910ea2fdb5297c6964ba26ffa8761a5adb84574b
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-24 16:53:20 +0200

    express.static("images) changed to process env + express.static("uploads"))

--- back/config/app.js
+++ back/config/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require("./../db/mongo");                  // Connexion à la base de données MongoDB
 
 app.use(cors());
 app.use(express.json());      // Middleware pour parser le corps des requêtes en JSON
-app.use('/images', express.static('images'));  //✅ CORRECTION (choisissez une option) : 'uploads' au lieu de 'images'. Middleware pour servir les fichiers statiques du dossier 'images'  
+app.use('/' + process.env.IMAGES_FOLDER_PATH, express.static('uploads'));  //✅ CORRECTION (choisissez une option) : 'uploads' au lieu de 'images'. Middleware pour servir les fichiers statiques du dossier 'images'  
 
 module.exports = { app };
