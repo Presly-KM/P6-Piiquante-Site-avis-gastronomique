85bf8b00d7c8737fe3420e1398496412c6276306
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-28 21:59:23 +0100

    likeSauce function temporary before testing

--- back/controllers/sauces.controller.js
+++ back/controllers/sauces.controller.js
@@ -9,6 +9,41 @@ saucesRouter.get("/", getSauces);
 saucesRouter.post("/", checkToken, upload.single("image"), postSauces);    // Utilisation de multer pour gérer l'upload d'une seule image avec le champ 'image' du formulaire. "single" signifie qu'on attend un seul fichier (et non plusieurs).
 saucesRouter.delete("/:id", checkToken, deleteSauce);
 saucesRouter.put("/:id", checkToken, upload.single("image"), putSauce); // Middleware pour vérifier le token JWT avant de permettre la mise à jour d'une sauce.
+saucesRouter.post(":id/like", checkToken, likeSauce);
+
+
+async function likeSauce(req, res) {
+    const id = req.params.id;
+    const like = req.body.like;
+    const userId = req.tokenPayload.userId;             // Récupération de l'ID utilisateur depuis le token JWT. On ne fait pas confiance au client pour nous envoyer l'ID utilisateur dans le corps de la requête. Car un utilisateur malveillant pourrait essayer de liker une sauce en se faisant passer pour un autre utilisateur en envoyant un userId différent dans le corps de la requête.
+    const sauce = await Sauce.findById(id);
+    if (sauce == null) {
+        res.status(404).send("Sauce non trouvée");
+        return;
+    }
+    if (like === 1) {
+        if (!sauce.usersLiked.includes(userId)) {
+            sauce.usersLiked.push(userId);
+            sauce.likes += 1;
+        }
+    } else if (like === -1) {
+        if (!sauce.usersDisliked.includes(userId)) {
+            sauce.usersDisliked.push(userId);
+            sauce.dislikes += 1;
+        }
+    } else if (like === 0) {
+        if (sauce.usersLiked.includes(userId)) {
+            sauce.usersLiked = sauce.usersLiked.filter(id => id !== userId);
+            sauce.likes -= 1;
+        }
+        if (sauce.usersDisliked.includes(userId)) {
+            sauce.usersDisliked = sauce.usersDisliked.filter(id => id !== userId);
+            sauce.dislikes -= 1;
+        }
+    }
+    await sauce.save();
+    res.send("Like/dislike mis à jour avec succès");
+}
 
 async function putSauce(req, res) {
     const id = req.params.id;
