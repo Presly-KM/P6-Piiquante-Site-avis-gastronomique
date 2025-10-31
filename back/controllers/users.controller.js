c888c9ec5fef8fe9fdda218b48b1acf6c80f08ee
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-31 19:03:15 +0100

    add try / catch

--- back/controllers/users.controller.js
+++ back/controllers/users.controller.js
@@ -11,51 +11,55 @@ async function signUp(req, res) {
     const email = req.body.email;                     // On extrait l'email du corps de la requête.
     const password = req.body.password;               // On extrait le mot de passe du corps de la requête.
 
-    const userInDb = await User.findOne({
-        email: email
-    });
-    if (userInDb != null) {
-        res.status(400).send("Utilisateur déjà existant avec cet email."); // Si un utilisateur avec le même email existe déjà, on renvoie une erreur 400 (Bad Request) au client.
-        return;                                                            // On arrête l'exécution de la fonction.
-    }
-    const user = {                                                         // On crée un nouvel objet utilisateur avec l'email et le mot de passe fournis.
-        email: email,
-        password: hashPassword(password)
-    };
     try {
+        const userInDb = await User.findOne({
+            email: email
+        });
+        if (userInDb != null) {
+            res.status(400).send("Utilisateur déjà existant avec cet email."); // Si un utilisateur avec le même email existe déjà, on renvoie une erreur 400 (Bad Request) au client.
+            return;                                                            // On arrête l'exécution de la fonction.
+        }
+        const user = {                                                         // On crée un nouvel objet utilisateur avec l'email et le mot de passe fournis.
+            email: email,
+            password: hashPassword(password)
+        };
+
         await User.create(user);                                             // On crée un nouvel utilisateur dans la base de données MongoDB. 
+        res.status(201).json({                                               // On renvoie une réponse 201 (Created) au client avec un message de succès.
+            message: "Inscription réussie !"
+        })
     } catch (e) {                                                         // Si une erreur survient lors de la création de l'utilisateur dans la base de données, on la capture.
         console.error(e);
         res.status(500).send("Erreur serveur lors de la création de l'utilisateur.");
-        return;
     }
-    res.status(201).json({                                               // On renvoie une réponse 201 (Created) au client avec un message de succès.
-        message: "Inscription réussie !"
-    });
 }
 
 async function login(req, res) {
     const body = req.body;
     console.log("body:", body);
+    try {
+        const userInDb = await User.findOne({                     // Ici, on cherche dans la base de données un utilisateur avec l'email que l'on a reçu dans le corps de la requête c'est à dire l'email qui a été tapé dans le formulaire de login.
+            email: body.email
+        });
+        if (userInDb == null) {
+            res.status(401).send("Mauvais email");
+            return;
+        }
+        const passwordInDb = userInDb.password;
+        if (!isPasswordCorrect(req.body.password, passwordInDb)) {    // (!isPasswordCorrect = si le mot de passe ne correspond pas) -> Si le mot de passe entré par l'utilisateur dans le champ de saisie (req.body.password) correspond au mot de passe stocké dans la base de données et qui est hashé (passwordInDb).
+            res.status(401).send("Mauvais mot de passe");             // Si le mot de passe ne correspond pas, on renvoie une erreur 401 (Unauthorized) au client.
+            return;
+        }
 
-    const userInDb = await User.findOne({                     // Ici, on cherche dans la base de données un utilisateur avec l'email que l'on a reçu dans le corps de la requête c'est à dire l'email qui a été tapé dans le formulaire de login.
-        email: body.email
-    });
-    if (userInDb == null) {
-        res.status(401).send("Mauvais email");
-        return;
+        // SIMULATION - À remplacer plus tard par la vraie logique
+        res.status(200).json({
+            userId: userInDb._id,
+            token: generateToken(userInDb._id)
+        });
+    } catch (e) {
+        console.error(e);
+        res.status(500).send("Erreur serveur lors de la connexion de l'utilisateur.");
     }
-    const passwordInDb = userInDb.password;
-    if (!isPasswordCorrect(req.body.password, passwordInDb)) {    // (!isPasswordCorrect = si le mot de passe ne correspond pas) -> Si le mot de passe entré par l'utilisateur dans le champ de saisie (req.body.password) correspond au mot de passe stocké dans la base de données et qui est hashé (passwordInDb).
-        res.status(401).send("Mauvais mot de passe");             // Si le mot de passe ne correspond pas, on renvoie une erreur 401 (Unauthorized) au client.
-        return;
-    }
-
-    // SIMULATION - À remplacer plus tard par la vraie logique
-    res.status(200).json({
-        userId: userInDb._id,
-        token: generateToken(userInDb._id)
-    });
 }
 
 function generateToken(idInDb) {                                  // Ici, on génère un token JWT en utilisant en paramètre l'ID de l'utilisateur stocké dans la base de données.
