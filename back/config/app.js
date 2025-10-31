6a731f8c1dfb7e0fcac993ad71ac88bf63d72115
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-31 16:21:01 +0100

    add IMAGES_FOLDER

--- back/config/app.js
+++ back/config/app.js
@@ -5,6 +5,6 @@ require("./../db/mongo.js");                  // Connexion à la base de donnée
 
 app.use(cors());
 app.use(express.json());      // Middleware pour parser le corps des requêtes en JSON
-app.use('/' + process.env.IMAGES_FOLDER_PATH, express.static(process.env.IMAGES_FOLDER));  //✅ CORRECTION (choisissez une option) : 'uploads' au lieu de 'images'. Middleware pour servir les fichiers statiques du dossier 'images'  
+app.use('/' + process.env.IMAGES_PUBLIC_URL, express.static(process.env.IMAGES_FOLDER));  //✅ CORRECTION (choisissez une option) : 'uploads' au lieu de 'images'. Middleware pour servir les fichiers statiques du dossier 'images'  
 
 module.exports = { app };
