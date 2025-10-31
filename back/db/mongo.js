ecb89c835c8cb27ea2376088e4fb596ec5f8399b
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-10-31 15:55:39 +0100

    require dotenv.  moved to server.js file

--- back/db/mongo.js
+++ back/db/mongo.js
@@ -1,4 +1,3 @@
-require('dotenv').config();                           // Charger les variables d'environnement depuis le fichier .env
 const mongoose = require('mongoose');
 
 const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_DOMAIN}`; // Mise en place de l'url pour mongo. "Process.env" vient activer les données mises dans le fichier ".env" et qu'on souhaite ne pas afficher aux yeux du public (à savoir le nom d'utilisateur, le mot de passe et le domaine)
