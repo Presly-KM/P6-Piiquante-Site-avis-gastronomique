1c3d50698d95e799f2b41dca520f0ac61e91b310
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-11-02 23:10:59 +0100

    comments

--- back/server.js
+++ back/server.js
@@ -1,7 +1,7 @@
-require('dotenv').config();                           // Charger les variables d'environnement depuis le fichier .env
-const { app } = require('./config/app');        // Importation de l'application Express depuis le fichier 'app.js'
-const { usersRouter } = require("./controllers/users.controller");
-const { saucesRouter } = require("./controllers/sauces.controller");
+require('dotenv').config();                                                         // Charger les variables d'environnement depuis le fichier .env
+const { app } = require('./config/app');                                            // Importation de l'application Express depuis le fichier 'app.js'
+const { usersRouter } = require("./controllers/users.controller");                  // Importation du routeur des utilisateurs
+const { saucesRouter } = require("./controllers/sauces.controller");                // Importation du routeur des sauces
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,6 +10,6 @@ app.get("/", (req, res) => res.send("Serveur de Piiquante entrain de tourner !")
 app.use("/api/auth", usersRouter);
 app.use("/api/sauces", saucesRouter);
 
-app.listen(PORT, function () {
+app.listen(PORT, function () {                                                      // Démarrage du serveur sur le port spécifié
     console.log(`🟢 Serveur de Piiquante en écoute sur le port:${PORT}`);
 });
