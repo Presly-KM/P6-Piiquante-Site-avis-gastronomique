5f831ab0c756fb0973811670e046bbc7e350ed23
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-11-02 23:13:57 +0100

    add comments (require monggose)

--- back/db/mongo.js
+++ back/db/mongo.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');                                                                      // Importation de Mongoose pour interagir avec MongoDB
 
 const DB_URL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_DOMAIN}`; // Mise en place de l'url pour mongo. "Process.env" vient activer les données mises dans le fichier ".env" et qu'on souhaite ne pas afficher aux yeux du public (à savoir le nom d'utilisateur, le mot de passe et le domaine)
 console.log("DB_URL:", DB_URL);
