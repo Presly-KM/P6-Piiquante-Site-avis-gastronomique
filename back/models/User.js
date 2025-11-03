77322eda6805672d3e3050f973d5ceaa65d82f32
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-11-03 11:53:33 +0100

    Add comments

--- back/models/User.js
+++ back/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({                // Définition du schéma Mongoose pour les utilisateurs. Cela va structurer les documents de la collection "users" dans MongoDB.
     email: String,
     password: String
 });
