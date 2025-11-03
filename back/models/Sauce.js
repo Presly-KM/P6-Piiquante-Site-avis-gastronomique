b51ae59150a343fe78b20dbf38a997270626b3d3
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-11-03 11:53:10 +0100

    add comments

--- back/models/Sauce.js
+++ back/models/Sauce.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const SauceSchema = new mongoose.Schema({
+const SauceSchema = new mongoose.Schema({                                      // Ici, on définit le schéma Mongoose pour les sauces. Cela va permettre de structurer les documents de la collection "sauces" dans MongoDB.
     userId: String,
     name: String,
     manufacturer: String,
@@ -8,7 +8,7 @@ const SauceSchema = new mongoose.Schema({
     mainPepper: String,
     imageUrl: String,
     heat: Number,
-    likes: { type: Number, default: 0 },
+    likes: { type: Number, default: 0 },                                      // Initialisation des champs likes et dislikes avec une valeur par défaut de 0. Type et Default sont des options de Mongoose pour définir le type de données et la valeur par défaut d'un champ. Cela permet de s'assurer que lorsqu'une nouvelle sauce est créée sans spécifier ces champs (undefined), ils seront automatiquement initialisés à 0.
     dislikes: { type: Number, default: 0 },
     usersLiked: { type: [String], default: [] },
     usersDisliked: { type: [String], default: [] }
