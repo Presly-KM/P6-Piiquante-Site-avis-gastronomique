cf7b62a09947498b89fc0b9f8e91ff9c2b1217b6
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-11-03 11:48:24 +0100

    Add comments

--- back/controllers/users.controller.js
+++ back/controllers/users.controller.js
@@ -1,15 +1,15 @@
 const { User } = require("../models/User");           // Importation du modèle User pour interagir avec la collection des utilisateurs dans la base de données MongoDB.
 const bcrypt = require("bcrypt");                     // Importation de la bibliothèque bcrypt pour le hachage des mots de passe.
 const express = require("express");
-const jwt = require("jsonwebtoken");                 // Importation de la bibliothèque jsonwebtoken pour la gestion des tokens JWT.
+const jwt = require("jsonwebtoken");                  // Importation de la bibliothèque jsonwebtoken pour la gestion des tokens JWT.
 
-const usersRouter = express.Router();
+const usersRouter = express.Router();                 // Création d'un routeur Express pour gérer les routes liées aux utilisateurs.
 usersRouter.post("/signup", signUp);
 usersRouter.post("/login", login);
 
 async function signUp(req, res) {
-    const email = req.body.email;                     // On extrait l'email du corps de la requête.
-    const password = req.body.password;               // On extrait le mot de passe du corps de la requête.
+    const email = req.body.email;                                              // On extrait l'email du corps de la requête.
+    const password = req.body.password;                                        // On extrait le mot de passe du corps de la requête.
 
     try {
         const userInDb = await User.findOne({
@@ -18,17 +18,19 @@ async function signUp(req, res) {
         if (userInDb != null) {
             res.status(400).send("Utilisateur déjà existant avec cet email."); // Si un utilisateur avec le même email existe déjà, on renvoie une erreur 400 (Bad Request) au client.
             return;                                                            // On arrête l'exécution de la fonction.
-        }
-        const user = {                                                         // On crée un nouvel objet utilisateur avec l'email et le mot de passe fournis.
-            email: email,
-            password: hashPassword(password)
-        };
+        } else {
 
-        await User.create(user);                                             // On crée un nouvel utilisateur dans la base de données MongoDB. 
-        res.status(201).json({                                               // On renvoie une réponse 201 (Created) au client avec un message de succès.
-            message: "Inscription réussie !"
-        })
-    } catch (e) {                                                         // Si une erreur survient lors de la création de l'utilisateur dans la base de données, on la capture.
+            const user = {                                                     // On crée un nouvel objet "utilisateur" avec l'email et le mot de passe fournis.
+                email: email,
+                password: hashPassword(password)
+            };
+
+            await User.create(user);                                           // On crée un nouvel utilisateur dans la base de données MongoDB. 
+            res.status(201).json({                                             // On renvoie une réponse 201 (Created) au client avec un message de succès.
+                message: "Inscription réussie !"
+            });
+        }
+    } catch (e) {                                                              // Si une erreur survient lors de la création de l'utilisateur dans la base de données, on la capture.
         console.error(e);
         res.status(500).send("Erreur serveur lors de la création de l'utilisateur.");
     }
@@ -38,7 +40,7 @@ async function login(req, res) {
     const body = req.body;
     console.log("body:", body);
     try {
-        const userInDb = await User.findOne({                     // Ici, on cherche dans la base de données un utilisateur avec l'email que l'on a reçu dans le corps de la requête c'est à dire l'email qui a été tapé dans le formulaire de login.
+        const userInDb = await User.findOne({                                  // Ici, on cherche dans la base de données un utilisateur avec l'email que l'on a reçu dans le corps de la requête c'est à dire l'email qui a été tapé dans le formulaire de login.
             email: body.email
         });
         if (userInDb == null) {
@@ -46,8 +48,8 @@ async function login(req, res) {
             return;
         }
         const passwordInDb = userInDb.password;
-        if (!isPasswordCorrect(req.body.password, passwordInDb)) {    // (!isPasswordCorrect = si le mot de passe ne correspond pas) -> Si le mot de passe entré par l'utilisateur dans le champ de saisie (req.body.password) correspond au mot de passe stocké dans la base de données et qui est hashé (passwordInDb).
-            res.status(401).send("Mauvais mot de passe");             // Si le mot de passe ne correspond pas, on renvoie une erreur 401 (Unauthorized) au client.
+        if (!isPasswordCorrect(req.body.password, passwordInDb)) {             // (!isPasswordCorrect = si le mot de passe ne correspond pas) -> Si le mot de passe entré par l'utilisateur dans le champ de saisie (req.body.password) correspond au mot de passe stocké dans la base de données et qui est hashé (passwordInDb).
+            res.status(401).send("Mauvais mot de passe");                      // Si le mot de passe ne correspond pas, on renvoie une erreur 401 (Unauthorized) au client.
             return;
         }
 
@@ -62,23 +64,23 @@ async function login(req, res) {
     }
 }
 
-function generateToken(idInDb) {                                  // Ici, on génère un token JWT en utilisant en paramètre l'ID de l'utilisateur stocké dans la base de données.
+function generateToken(idInDb) {                                      // Ici, on génère un token JWT en utilisant en paramètre l'ID de l'utilisateur stocké dans la base de données.
     const payload = {
         userId: idInDb
     };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {                  // Ici, on signe le token avec une clé secrète "SYLABUS" (à remplacer par une clé plus sécurisée en production) et on définit une durée de validité pour le token.
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {         // Ici, on signe le token avec une clé secrète "SYLABUS" (à remplacer par une clé plus sécurisée en production) et on définit une durée de validité pour le token.
         expiresIn: "1d"
     });
     return token;
 }
 
-function hashPassword(password) {
+function hashPassword(password) {                                     // Ici, on hash le mot de passe en utilisant bcrypt avant de le stocker dans la base de données.
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
     return hash;
 }
 
-function isPasswordCorrect(password, hash) {                    // Ici, on compare le mot de passe fourni avec le mot de passe hashé stocké dans la base de données. 
+function isPasswordCorrect(password, hash) {                         // Ici, on compare le mot de passe fourni avec le mot de passe hashé stocké dans la base de données. 
     return bcrypt.compareSync(password, hash);
 }
 
