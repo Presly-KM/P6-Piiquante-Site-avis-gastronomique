dc00769e09ef63db79b06dbbba8aa1e5c2e01fcc
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-11-10 15:24:34 +0100

    Add comment cors

--- back/config/app.js
+++ back/config/app.js
@@ -3,7 +3,7 @@ const cors = require('cors');                                                   
 const app = express();                                                                    // Création de l'application Express
 require("./../db/mongo.js");                                                              // Connexion à la base de données MongoDB
 
-app.use(cors());
+app.use(cors());                                                                          // Activation de CORS pour toutes les routes de l'application. Cela permet à des applications front-end hébergées sur des domaines différents d'accéder aux ressources de ce serveur.
 app.use(express.json());                                                                  // Middleware pour parser le corps des requêtes en JSON
 app.use('/' + process.env.IMAGES_PUBLIC_URL, express.static(process.env.IMAGES_FOLDER));  //✅ CORRECTION (choisissez une option) : 'uploads' au lieu de 'images'. Middleware pour servir les fichiers statiques du dossier 'images'  
 
